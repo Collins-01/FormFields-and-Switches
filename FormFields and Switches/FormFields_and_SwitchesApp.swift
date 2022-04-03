cb0159aff23ad3ec448192a129e5bd922f920ff1
Collins Oriakhi <collins.oriakhi@itara.co>
2022-04-03 10:23:25 +0100

    Initial Commit

---
+++ FormFields and Switches/FormFields_and_SwitchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormFields_and_SwitchesApp.swift
+//  FormFields and Switches
+//
+//  Created by Oriakhi Collins on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FormFields_and_SwitchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
